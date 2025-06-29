6f80911bfe24d4f72c6c69a8888e043f17b36a89
Shubhadeep <shubhadeep.pal.sde@gmail.com>
2025-06-29 22:45:32 +0530

    first version commit

---
+++ src/main/java/simpleMavenProject/test/Testing.java
@@ -0,0 +1,10 @@
+package simpleMavenProject.test;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello This is my first project");
+	}
+
+}
